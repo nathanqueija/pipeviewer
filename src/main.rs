59bc2b7ef42047696b87d15e8a7bf038b4ddf9f4
Nathan <nathanqcam@gmail.com>
2020-04-09 23:18:11 +0200

    Add loop to handle multiple stdin reads

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,28 @@ use std::io::{self, Read, Write};
 const CHUNK_SIZE:usize = 16 * 1024;
 
 fn main() {
-    let mut buffer = [0; CHUNK_SIZE];
 
-    // Opening the standard input of the current process. In this case is the terminal.
-    // The standard input is the keyboard.
-    // The read function pulls bytes from the stdin source into the specified buffer and returns
-    // how many bytes were read
-    // In this case the buffer is an array
-    let num_read = io::stdin().read( &mut buffer).unwrap();
+    let mut total_bytes = 0;
 
-    eprintln!("num_read: {}", num_read);
+    loop{
+        let mut buffer = [0; CHUNK_SIZE];
 
-    io::stdout().write_all(&buffer[..num_read]).unwrap();
+        // Opening the standard input of the current process. In this case is the terminal.
+        // The standard input is the keyboard.
+        // The read function pulls bytes from the stdin source into the specified buffer and returns
+        // how many bytes were read
+        // In this case the buffer is an array
+        let num_read = match io::stdin().read( &mut buffer) {
+            Ok(0) => break,
+            Ok(x) => x,
+            Err(_) => break,
+        };
+
+        total_bytes += num_read;
+
+        io::stdout().write_all(&buffer[..num_read]).unwrap();
+    }
+
+    eprintln!("Total bytes read: {}", total_bytes);
+
 }
