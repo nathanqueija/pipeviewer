8b6f29b415a59be1a3b12295308d12b753c121c4
Nathan <nathanqcam@gmail.com>
2020-04-09 22:57:00 +0200

    Add initial stdin read and buffer feed

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::io::{self, Read, Write};
+
+// 16kb max
+const CHUNK_SIZE:usize = 16 * 1024;
+
 fn main() {
-    println!("Hello, world!");
+    let mut buffer = [0; CHUNK_SIZE];
+
+    // Opening the standard input of the current process. In this case is the terminal.
+    // The standard input is the keyboard.
+    // The read function pulls bytes from the stdin source into the specified buffer and returns
+    // how many bytes were read
+    // In this case the buffer is an array
+    let num_read = io::stdin().read( &mut buffer).unwrap();
+
+    eprintln!("num_read: {}", num_read);
+
+    io::stdout().write_all(&buffer[..num_read]).unwrap();
 }
