114d519bbe7a607103d568eaed3ed797f1fc59d4
Nathan <nathanqcam@gmail.com>
2020-04-10 10:44:44 +0200

    handle errors

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,16 @@
 use std::env;
-use std::io::{self, Read, Write};
+use std::io::{self, ErrorKind, Read, Result, Write};
 
 // 16kb max
-const CHUNK_SIZE:
-usize = 16 * 1024;
+const CHUNK_SIZE: usize = 16 * 1024;
 
-fn main() {
-    if true == true {}
+fn main() -> Result<()> {
     let silent = !env::var("PV_SILENT").unwrap_or_default().is_empty();
 
     let mut total_bytes = 0;
+    let mut buffer = [0; CHUNK_SIZE];
 
     loop {
-        let mut buffer = [0; CHUNK_SIZE];
-
         // Opening the standard input of the current process. In this case is the terminal.
         // The standard input is the keyboard.
         // The read function pulls bytes from the stdin source into the specified buffer and returns
@@ -27,10 +24,25 @@ fn main() {
 
         total_bytes += num_read;
 
-        io::stdout().write_all(&buffer[..num_read]).unwrap();
+        if !silent {
+            eprintln!("\r{}", total_bytes);
+        }
+
+        // If you use the ? syntax it means that if this operations returns an Error
+        // it will be returned from this function
+        // io::stdout().write_all(&buffer[..num_read])?
+
+        if let Err(e) = io::stdout().write_all(&buffer[..num_read]) {
+            if e.kind() == ErrorKind::BrokenPipe {
+                break;
+            }
+            return Err(e);
+        }
     }
 
     if !silent {
-        eprintln!("Total bytes read: {}", total_bytes);
+        eprintln!("\r{}", total_bytes);
     }
+
+    Ok(())
 }
